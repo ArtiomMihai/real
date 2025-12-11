73ab673db6def63de723b9ee96f68701c118bf04
ArtiomMihai <145434015+ArtiomMihai@users.noreply.github.com>
2025-12-11 02:55:16 +0200

    Add files via upload

---
+++ RealSteel/src/test/java/com/example/RealSteel/RealSteelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealSteel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealSteelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
